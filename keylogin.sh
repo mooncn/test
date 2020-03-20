9927965405512bddb98d3ac00df6db683a458441
mooncn <qkmyyx@163.com>
2020-03-20 14:14:34 +0800

    remove auto replace the rpm sources
    prevent to cause something wrong in the oversea's vps servers.

--- keylogin.sh
+++ keylogin.sh
@@ -5,9 +5,9 @@
 #Purpose: one key for replace the SSH Authorization method .
 setuppublickey()
 {
-rpm -ivh http://xb1028-10066197.cos.myqcloud.com/1028/epel-release-7-5.noarch.rpm
-rpm -ivh http://xb1028-10066197.cos.myqcloud.com/1028/remi-release-7.rpm
-yum install nload -y
+#rpm -ivh http://xb1028-10066197.cos.myqcloud.com/1028/epel-release-7-5.noarch.rpm
+#rpm -ivh http://xb1028-10066197.cos.myqcloud.com/1028/remi-release-7.rpm
+#yum install nload -y
 cd /root
 mkdir .ssh
 cd .ssh
